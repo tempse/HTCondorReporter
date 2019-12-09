bcdfb00083185e0b833542e4844031192bca1f9c
Sebastian Templ <sebastian.templ@cern.ch>
2019-12-09 23:37:23 +0100

    Add support for per-task reports, improvements
    - make use of the new condor_q report format on Lxplus, which
      reports on a per-task level now
    - add emojis to some reporter messages
    - minor restructuring and cleaning of the sent messages

--- Tools/python/HTCondorTools.py
+++ Tools/python/HTCondorTools.py
@@ -4,36 +4,64 @@ import re
 def read_condor_q(report):
     report_dict = {}
 
-    # extract scheduler
-    res = re.search(r"Schedd: (.*).cern.ch", report)
-    if res:
-        report_dict["schedd"] = res.group(1)
+    task_headers = []
+    switch_mode_detailed_task = False
 
-    # extract number of jobs in different categories
-    res = re.search(
-        r"(\d+) jobs; (\d+) completed, (\d+) removed, (\d+) idle, (\d+) running, (\d+) held, (\d+) suspended",
-        report,
-    )
-    if res:
-        report_dict["total_jobs"] = int(res.group(1))
-        report_dict["completed_jobs"] = int(res.group(2))
-        report_dict["removed_jobs"] = int(res.group(3))
-        report_dict["idle_jobs"] = int(res.group(4))
-        report_dict["running_jobs"] = int(res.group(5))
-        report_dict["held_jobs"] = int(res.group(6))
-        report_dict["suspended_jobs"] = int(res.group(7))
+    for line in report.split("\n"):
+        # extract the scheduler
+        if line.startswith("-- Schedd:"):
+            res = re.search(r"Schedd: (.*).cern.ch", line)
+            if res:
+                report_dict["schedd"] = res.group(1)
+
+        # extract the column headers of the tasks report
+        elif line.startswith("OWNER"):
+            switch_mode_detailed_task = True  # the following lines are tasks reports
+            for h in line.split():
+                task_headers.append(h)
+
+        # extract the values of a task report line
+        elif switch_mode_detailed_task:
+            if line != "\n" and line != "":
+                line_elements = line.split()
+                job_id = line_elements[1] + " " + line_elements[2]
+                submit_datetime = line_elements[3] + " " + line_elements[4]
+                report_dict[job_id] = {"SUBMITTED": submit_datetime}
+                for k,v in zip(task_headers[3:], line_elements[5:]):
+                    report_dict[job_id][k] = v
+            else:
+                # an empty line denotes the end of the task report(s)
+                switch_mode_detailed_task = False
+
+        else:
+            # extract cumulative numbers of jobs
+            res = re.search(
+                    r"Total for query: (\d+) jobs; (\d+) completed, (\d+) removed, (\d+) idle, (\d+) running, (\d+) held, (\d+) suspended",
+                report
+            )
+            report_dict["total_jobs"] = int(res.group(1))
+            report_dict["completed_jobs"] = int(res.group(2))
+            report_dict["removed_jobs"] = int(res.group(3))
+            report_dict["idle_jobs"] = int(res.group(4))
+            report_dict["running_jobs"] = int(res.group(5))
+            report_dict["held_jobs"] = int(res.group(6))
+            report_dict["suspended_jobs"] = int(res.group(7))
 
     return report_dict
 
 
 if __name__ == "__main__":
     condor_q_output = """
--- Schedd: bigbird36.cern.ch : <123.456.789.10:1112?... @ 08/03/19 09:22:47
-OWNER    BATCH_NAME                  SUBMITTED   DONE   RUN    IDLE  TOTAL JOB_IDS
-username CMD: condorExecutable.sh   8/3  09:08      _     74   2156   2233 158324.0 ... 158351.11
+-- Schedd: bigbird36.cern.ch : <123.456.789.10:1112?... @ 12/09/19 21:43:53
+OWNER    BATCH_NAME    SUBMITTED   DONE   RUN    IDLE  TOTAL JOB_IDS
+username ID: 704081  12/9  16:36    222    33     286    541 704081.0-285
+username ID: 704082  12/9  16:36    2     123     500    625 704081.0-499
 
-2230 jobs; 0 completed, 0 removed, 2156 idle, 74 running, 0 held, 0 suspended
+Total for query: 1166 jobs; 224 completed, 0 removed, 786 idle, 156 running, 0 held, 0 suspended 
+Total for username: 1166 jobs; 0 completed, 0 removed, 786 idle, 156 running, 0 held, 0 suspended 
+Total for all users: 5013 jobs; 2 completed, 0 removed, 3753 idle, 1224 running, 34 held, 0 suspended
 """
 
     condor_report = read_condor_q(condor_q_output)
-    print(condor_report)
+    for k in condor_report:
+        print("{}: {}".format(k, condor_report[k]))
--- Tools/scripts/condorReporter.py
+++ Tools/scripts/condorReporter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys, time
+import random
 import subprocess
 
 from HTCondorReporter.Tools.telegramNotifier import send
@@ -12,7 +13,7 @@ hostname = os.getenv("HOSTNAME")
 start_time = time.strftime("%d.%m.%Y %H:%M:%S", time.localtime())
 
 send(
-    "{} HTCondorReporter: Start monitoring Condor jobs of user {} on {}".format(
+    "\xE2\x8F\xB0 {} HTCondorReporter: Start monitoring Condor jobs of user {} on {}".format(
         start_time, user, hostname
     )
 )
@@ -21,11 +22,6 @@ report = subprocess.check_output(["condor_q"])
 report_dict = read_condor_q(report)
 schedd = report_dict["schedd"]
 total_jobs = report_dict["total_jobs"]
-send(
-    "{} HTCondorReporter: Monitoring {} jobs on scheduler {}".format(
-        start_time, total_jobs, schedd
-    )
-)
 
 cnt = 0
 while True:
@@ -35,29 +31,72 @@ while True:
     cnt += 1
     # send a status report once every hour
     if cnt % 12 == 0:
+        tasks = {
+            job_id: report_dict[job_id]
+            for job_id in report_dict
+            if job_id.startswith("ID:")
+        }
+        message = "\xE2\x8C\x9B HTCondorReporter: Monitoring {} jobs in {} tasks:".format(
+            report_dict["total_jobs"], len(tasks)
+        )
+        for task in tasks:
+            message += "\n\n\xE2\x9E\xA1 {} ({}) - DONE: {}, RUN: {}, IDLE: {}".format(
+                task,
+                tasks[task]["SUBMITTED"],
+                tasks[task]["DONE"],
+                tasks[task]["RUN"],
+                tasks[task]["IDLE"],
+            )
+            for s in tasks[task]:
+                if s not in ("HELD", "SUSPENDED"):
+                    continue
+                message += ", {}: {}".format(s, tasks[task][s])
+
+        message += "\n\nSUMMARY: Completed: {}, removed: {}, idle: {}, running: {}, held: {}, suspended: {}. Scheduler: {}".format(
+            report_dict["completed_jobs"],
+            report_dict["removed_jobs"],
+            report_dict["idle_jobs"],
+            report_dict["running_jobs"],
+            report_dict["held_jobs"],
+            report_dict["suspended_jobs"],
+            schedd,
+        )
+
+        send(message)
+
+    if report_dict["total_jobs"] == 0:
         send(
-            "HTCondorReporter (sched: {}): Monitoring {} jobs. Completed: {}, removed: {}, idle: {}, running: {}, held: {}, suspended: {}".format(
-                schedd,
-                report_dict["total_jobs"],
-                report_dict["completed_jobs"],
-                report_dict["removed_jobs"],
-                report_dict["idle_jobs"],
-                report_dict["running_jobs"],
-                report_dict["held_jobs"],
-                report_dict["suspended_jobs"],
+            "\xE2\x9C\x85 HTCondorReporter: Jobs finished running on scheduler {}".format(
+                schedd
             )
         )
-    if report_dict["total_jobs"] == 0:
-        send("HTCondorReporter: Jobs finished running on scheduler {}".format(schedd))
         break
 
     # end reporting after 1 week
     if cnt % 2016 == 0:
-        send("HTCondorReporter: Terminated after one week of running")
+        send("\xE2\x9D\x8C HTCondorReporter: Terminated after one week of running")
         sys.exit(1)
 
     time.sleep(300)  # sleep for 5 minutes
 
 
 end_time = time.strftime("%d.%m.%Y %H:%M:%S", time.localtime())
-send("HTCondorReporter: Finished monitoring jobs at {}".format(end_time))
+success_emojis = (
+    "\xF0\x9F\x9A\x80",
+    "\xF0\x9F\x99\x8C",
+    "\xE2\x9C\x8C",
+    "\xE2\x9C\x94",
+    "\xE2\x98\x95",
+    "\xF0\x9F\x8E\x89",
+    "\xF0\x9F\x91\x8C",
+    "\xF0\x9F\x91\x8D",
+    "\xF0\x9F\x91\x8F",
+    "\xF0\x9F\x91\x90",
+    "\xF0\x9F\x92\xAA",
+    "\xF0\x9F\x98\x8E",
+)
+send(
+    "{} HTCondorReporter: Finished monitoring jobs at {}".format(
+        random.choice(success_emojis), end_time
+    )
+)
